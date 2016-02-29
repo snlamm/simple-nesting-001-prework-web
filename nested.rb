fac2693be88231f2a81cfdb88a209d4a8fd8b16a
snlamm <shmuel.lamm@gmail.com>
2016-02-29 16:45:48 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +84,15 @@ def adding_matz
         }
      }
 
-    
+another_programmer = {
+  :yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP, C"]
+  }
+}
+
+programmer_hash = another_programmer.merge(programmer_hash)
+
 end
 
 def changing_alan
@@ -100,7 +112,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
      
      
 end
@@ -122,7 +136,8 @@ def adding_to_dennis
         }
      }
 
-     
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly" 
+  programmer_hash
 end
 
 
